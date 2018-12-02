8661d546e7befad11afaef2d889128fc5140efb7
StrScr <andres.mejia@unitec.edu>
2018-12-01 18:50:49 -0600

    Polish models

--- DesignRecognition/models.py
+++ DesignRecognition/models.py
@@ -15,14 +15,14 @@ class Post(models.Model):
         (HOSTILE, 'Hostile'),
     )
 
-    image = models.ImageField(upload_to='pics/%Y/%m/%d/')
-    desc = models.TextField('Description', max_length=280)
-    design = models.CharField(max_length=1, choices=DESIGN_TYPES)
-    score = models.FloatField(default=1.0)
-    geotag = models.CharField(max_length=56) # unsure
-    timestamp = models.DateTimeField('Date Added', auto_now_add=True)
+    image = models.ImageField(upload_to='pics/%Y/%m/%d/', null=False)
+    desc = models.TextField('Description', max_length=280, null=True)
+    design = models.CharField(max_length=1, choices=DESIGN_TYPES, null=False)
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, blank=True, null=True)
-    lens = models.ForeignKey("Lens", on_delete=models.SET_NULL, blank=True, null=True)
+    lens = models.ForeignKey('Lens', on_delete=models.SET_NULL, blank=True, null=True)
+    geotag = models.CharField(max_length=56) #! unsure
+    score = models.FloatField(default=1.0)
+    timestamp = models.DateTimeField('Date Added', auto_now_add=True)
 
     def __str__(self):
         return '{} on {}'.format(self.design, self.timestamp)
@@ -55,8 +55,8 @@ class Correction(models.Model):
 
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    agrees = models.BooleanField()
-    replace = models.CharField('Suggested Correction', max_length=1, choices=DESIGN_TYPES)
+    agrees = models.BooleanField(default=True)
+    replace = models.CharField('Suggested Correction', max_length=1, choices=DESIGN_TYPES, null=True)
     timestamp = models.DateTimeField('Date Added', auto_now_add=True)
 
     def __str__(self):
