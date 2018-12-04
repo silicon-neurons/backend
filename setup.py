952734f74fcf88e7a63d2028b74c2d12c5482b43
StrScr <andres.mejia@unitec.edu>
2018-12-04 08:02:56 -0600

    Specify more information in setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 
-setup(name='DesignRecognitionBackend',
-      version='0.1'
-     )
+setup(
+    name='DesignRecognitionBackend',
+    version='0.1',
+    description='Backend for a design recognition project.',
+    author='StrScr',
+    author_email='andres.mejia@unitec.edu',
+    packages=['DesignRecognition'],
+)
