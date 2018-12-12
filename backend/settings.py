37c88466d95ef35187fcef9dfc5a08a5aaff7758
dariovillalta <dario.villalta@gmail.com>
2018-12-11 23:13:57 -0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bjbtsoif',
+        'USER': 'bjbtsoif',
+        'PASSWORD': 'Hq0e33p98G8R_5lfMItZhjIuyHeqaIo1',
+        'HOST': 'elmer.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
