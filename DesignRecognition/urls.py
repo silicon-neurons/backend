91e4a2006f8464d283a7fa757ab1e0b6ca1f5e31
Dario Villalta <dario.villata@gmail.com>
2018-12-15 18:51:29 -0600

    reverting

--- DesignRecognition/urls.py
+++ DesignRecognition/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostDetailView, LensListView, LocationLimitedListView
+from .views import PostListView, PostCreateView, PostDetailView, LensListView
 # TODO Add Correction
 
 urlpatterns = [
-    #? Is this the best way to send floats through URL? Maybe match exact pattern?
-    path('posts/<str:x1>/<str:y1>/<str:x2>/<str:y2>/', LocationLimitedListView.as_view(), name='posts-limited'),
     path('posts/', PostListView.as_view(), name='posts-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/', PostCreateView.as_view(), name='post-new'),
